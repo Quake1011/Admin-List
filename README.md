ca2d8b6ed738134751a6d4a8db17a14343f09ea3
Alexander Malik <malik101112@mail.ru>
2023-11-11 11:24:27 +1000

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Admin List
- Simple admin list plugin
+# Admin list
+Simple admin list plugin
+
+## Requirements
+- [Metamod](https://www.sourcemm.net/downloads.php/?branch=master)
+- [CounterStrikeSharp](https://github.com/roflmuffin/CounterStrikeSharp/releases/tag/v28) >= v28
+
+## Install
+- Drop folder `build/AdminList` to `addons/counterstrikesharp/plugins/`
+
+- Configuration file will be generated after the plugin is launched in the folder `addons/counterstrikesharp/plugins/AdminList`:
+	- `Config.json`
+	
+### Config.json
+```
+{
+	"ImmunityFlag": "@css/root",			// Do not display administrators with this flag
+	"ShowSelf": true,						// Do not take yourself out if the administrator [ true - on | false - off ]
+	"ShowFlag": "@css/ban",					// Administrator flag to display in the list
+}
+```
+
+## About possible problems, please let me know: 
+[<img src="https://i.ibb.co/LJz83MH/a681b18dd681f38e599286a07a92225d.png" width="15.3%"/>](https://discordapp.com/users/858709381088935976/)
+[<img src="https://i.ibb.co/tJTTmxP/vk-process-mining.png" width="15.3%"/>](https://vk.com/bgtroll)
+[<img src="https://i.ibb.co/VjhryGb/png-transparent-brand-logo-steam-gump-s.png" width="15.3%"/>](https://hlmod.ru/members/palonez.92448/)
+[<img src="https://i.ibb.co/xHZPN0g/s-l500.png" width="15.3%"/>](https://steamcommunity.com/id/comecamecame)
+[<img src="https://i.ibb.co/S0LyzmX/tg-process-mining.png" width="16.3%"/>](https://t.me/ArrayListX)
+[<img src="https://i.ibb.co/Tb2gprD/2056021.png" width="15.3%"/>](https://github.com/Quake1011)
---
+++ src/AdminList/AdminList.cs
@@ -0,0 +1,57 @@
+﻿using System.Text.Json;
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace AdminList;
+
+public abstract class AdminList : BasePlugin
+{
+    public override string ModuleName => "Admin List";
+    public override string ModuleVersion => "1.0";
+    public override string ModuleAuthor => "Quake1011";
+
+    private static Config? _config;
+
+    public override void Load(bool hl)
+    {
+        var configPath = Path.Join(ModuleDirectory, "Config.json");
+        if (!File.Exists(configPath))
+        {
+            var data = new Config() { ImmunityFlag = "@css/root", ShowSelf = true, ShowFlag = "@css/ban" };
+            File.WriteAllText(configPath, JsonSerializer.Serialize(data, new JsonSerializerOptions{ WriteIndented = true, Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping }));
+            _config = data;
+        }
+        else _config = JsonSerializer.Deserialize<Config>(File.ReadAllText(configPath));        
+    }
+
+    [ConsoleCommand("admins", "Prints admins list")]
+    public void OnCommand(CCSPlayerController? activator, CommandInfo command)
+    {
+        var admins = 1;
+        for (var i = 0; i <= Server.MaxPlayers; i++)
+        {
+            var player = new CCSPlayerController(NativeAPI.GetEntityFromIndex(i));
+            
+            if (player is not { IsBot: false, IsValid: true}) continue;
+            if (!(_config?.ShowFlag != null && AdminManager.PlayerHasPermissions(player, _config.ShowFlag)) || AdminManager.PlayerHasPermissions(player, _config.ImmunityFlag!)) continue;
+
+            if (player == activator) 
+                if (_config.ShowSelf) activator.PrintToChat($"[#{admins}] {ChatColors.LightRed}{player.PlayerName}");
+            else activator.PrintToChat($"[#{admins}] {ChatColors.LightRed}{player.PlayerName}");
+            
+            admins++;
+        }
+    }
+    private class Config
+     {
+         public string? ImmunityFlag { get; init; }
+         public bool ShowSelf { get; init; }
+         public string? ShowFlag { get; init; }
+     }
+}
+
+
